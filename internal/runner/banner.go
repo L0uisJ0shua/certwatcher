6864d202a051d9f815114e9ae5f23dd9bd94e349
drfabiocastro <drfabiocastrosani@gmail.com>
2023-02-11 19:28:02 -0300

    Add Config Version

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -1,10 +1,19 @@
 package runner
 
 import (
-	"fmt"
-	"github.com/projectdiscovery/gologger"
+ "fmt"
+ "github.com/projectdiscovery/gologger"
+ "pkg/config"
 )
 
-func Run() {
-	// Display the Banner of Application
+var banner = fmt.Sprintf(`
+             _             _       _
+ ___ ___ ___| |_ _ _ _ ___| |_ ___| |_ ___ ___ 
+|  _| -_|  _|  _| | | | .'|  _|  _|   | -_|  _|
+|___|___|_| |_| |_____|__,|_| |___|_|_|___|_|  
+                                              v%s`, config.Version)
+
+func Banner() {
+	gologger.Print().Msgf("%s\n", banner)
+	gologger.Print().Msgf("\t\t\t\t%s.io\n\n", config.Name)
 }

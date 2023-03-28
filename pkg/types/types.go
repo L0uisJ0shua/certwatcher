31d18852d5595191c16d3a664ded3cd55895563c
drfabiocastro <drfabiocastrosani@gmail.com>
2023-03-28 03:28:52 -0300

    Remove Options Struct

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,37 +1,15 @@
 package types
 
-import "github.com/projectdiscovery/goflags"
-
 type Message struct {
-    Domain       string
-    Domains      []string
+    Domain         string
+    Domains        []string
     Issuer         string
     Source         string
     SubjectAltName string
 }
 
 type Protocols struct {
-    DNS         string
-    SSL         string
-    Log         string
-}
-
-type Options struct {
-    Templates      goflags.StringSlice
-    Validate       bool
-    Headless       bool
-    PageTimeout    int
-    PageScreenShot bool
-    Verbose        bool
-    Debug          bool
-    Version        bool
-    Retries        int
-    Timeout        int
-}
-
-func DefaultOptions() *Options {
-    return &Options{
-        Timeout: 5,
-        Retries: 1,
-    }
+    DNS string
+    SSL string
+    Log string
 }
